0d9a383294791945971d0c1f52619d0c7d91caf0
Jitendra Adhikari <jiten.adhikary@gmail.com>
2018-08-07 20:54:11 +0700

    Well that doesnt need mock?

--- tests/unit/RouterTest.php
+++ tests/unit/RouterTest.php
@@ -1,35 +1,30 @@
 <?php
 
-class RouterTest extends PHPUnit_Framework_TestCase {
-
+class RouterTest extends PHPUnit_Framework_TestCase
+{
     const TEST_PASSED = 'TEST PASSED';
 
-    public function response01(){
+    public function response01()
+    {
         return self::TEST_PASSED;
     }
 
     public function routesProvider()
     {
        return [
-            ['/', 'GET','\/', 'RouterTest@response01', self::TEST_PASSED], // Basic home page
-            ['/asd', 'GET','\/', 'RouterTest@response01', false], // 404 error
-            ['/page', 'GET','\/page', 'RouterTest@response01', self::TEST_PASSED],
-            ['/page/1', 'GET','\/page\/(.*)', function($a){ return $a; }, '1'], // Callback with params
+            ['/', 'GET', '/', 'RouterTest@response01', self::TEST_PASSED],         // Basic home page
+            ['/asd', 'GET', '/', 'RouterTest@response01', false],                  // 404 error
+            ['/page', 'GET', '/page', 'RouterTest@response01', self::TEST_PASSED],
+            ['/page/1', 'GET', '/page/(.*)', function($a) { return $a; }, '1'],    // Callback with params
         ];
     }
 
     public function getRouter($url)
     {
-             // Create a stub for the SomeClass class.
-        $stub = $this->getMockBuilder('\PHPico\Router')
-                     ->setMethods(array('base'))
-                     ->getMock();
+        $_SERVER['SCRIPT_NAME'] = '';
+        $_SERVER['REQUEST_URI'] = $url;
 
-        // Configure the stub.
-        $stub->method('base')
-             ->willReturn($url);
-
-        return $stub;
+        return new \PHPico\Router;
     }
 
     /**
@@ -41,9 +36,9 @@ class RouterTest extends PHPUnit_Framework_TestCase {
 
         $r = $this->getRouter($location);
         $result = $r->execute($regexp, $callable);
+
         $this->assertEquals($expected_result, $result);
     }
-
 
     public function locationsProvider()
     {
@@ -63,12 +58,13 @@ class RouterTest extends PHPUnit_Framework_TestCase {
     public function testDispatch($location, $expected_result)
     {
         $routes = [
-            '\/page\/(.*)' => function($a) { return $a; },
-            '\/pages' => 'RouterTest@response01',
-            '\/' => 'RouterTest@response01', // Basic home page
+            '/page/(.*)' => function($a) { return $a; },
+            '/pages' => 'RouterTest@response01',
+            '/' => 'RouterTest@response01', // Basic home page
         ];
 
         $r = $this->getRouter($location);
+
         $this->assertEquals($expected_result, $r->dispatch($routes));
     }
 }
